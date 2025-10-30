df136e02ab5c80b0be60d9431f3a78c182261d35
ac-bushrakhan <bushra.khan@armorcode.io>
2025-10-30 12:26:18 +0530

    Create package.json for project setup
    Added package.json with project metadata and dependencies.
    Dependabot

--- test/brackets/app(explorations)teacher.tsx
+++ test/brackets/app(explorations)teacher.tsx
@@ -1,1 +1,22 @@
-// Parentheses in path
+{
+  "name": "dependabot-vulnerability-example",
+  "version": "1.0.0",
+  "description": "A minimal project to demonstrate a Dependabot alert.",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "keywords": [
+    "dependabot",
+    "vulnerability",
+    "express"
+  ],
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "express": "4.15.2" 
+    
+    // 🛑 THIS IS THE VULNERABLE DEPENDENCY 
+    // express versions 4.15.0 - 4.15.2 have a known vulnerability.
+  }
+}// Parentheses in path
